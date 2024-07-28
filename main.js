0a7deb364321cc3f989cf3ef71af77f790f0cc33
anurmamedov <sdeveloperanar@gmail.com>
2024-07-28 16:41:52 -0400

    style css added

--- main.js
+++ main.js
@@ -1,7 +1,33 @@
-function showAlert(message) {
-    alert(message);
-  }
-  
-  window.onload = function() {
-    showAlert("The page has loaded!");
-  };
+document.addEventListener("DOMContentLoaded",function(){
+
+    document.querySelectorAll('.dropdown').forEach(item => {
+        item.addEventListener('click', function(event) {
+            event.stopPropagation();
+            item.classList.toggle('is-active' );
+        });
+    });
+
+    var amount = document.getElementById('amount');
+
+    
+
+
+    // var convert = document.getElementById('convert');
+    // convert.addEventListener('click', function() {
+    //     console.log('convert-button-isclicked');
+    //     console.log(amount.value);
+    // });
+
+    var buttons = document.querySelectorAll('.con-button');
+    // console.log(buttons)
+    for (let i = 0; i < buttons.length; i++ ){
+        buttons[i].addEventListener('click', function() {
+            console.log(buttons[i])
+        });        
+    }
+
+
+});
+
+
+
