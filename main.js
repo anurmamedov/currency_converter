ceccdbdc98608a263f1b1d8b61f6cafb74baf5f3
anurmamedov <sdeveloperanar@gmail.com>
2024-07-28 23:14:33 -0400

    convert amount added

--- main.js
+++ main.js
@@ -9,23 +9,27 @@ document.addEventListener("DOMContentLoaded",function(){
 
     var amount = document.getElementById('amount');
 
-    
 
+    var convert = document.getElementById('convert');
+    convert.addEventListener('click', function() {
+        convertAmount(amount.value)
 
-    // var convert = document.getElementById('convert');
-    // convert.addEventListener('click', function() {
-    //     console.log('convert-button-isclicked');
-    //     console.log(amount.value);
-    // });
+    });
 
-    var buttons = document.querySelectorAll('.con-button');
-    // console.log(buttons)
-    for (let i = 0; i < buttons.length; i++ ){
-        buttons[i].addEventListener('click', function() {
-            console.log(buttons[i])
-        });        
+    function convertAmount(number) {
+        console.log(number * 2)
+        fetch('https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_zHRgxYdqbaA38Aa8NdW0HxZY0T0natnrQDEOYZ1R&currencies=EUR%2CUSD%2CCAD')
+
+        .then(response => response.json())
+        .then(data => multiplyCurrency(data, number))
+        .catch(error => console.error('Error:',   
+            error));
+
+    };
+
+    function multiplyCurrency(data, number) {
+        console.log(data['data']['CAD'] * number)
     }
-
 
 });
 
